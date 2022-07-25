bb0bc69e1e9c3a065c274faeafe9c466b850e31e
SamGreat1 <samantha.greatorex@genesys.com>
2022-07-25 16:02:17 +0200

    loaded available resources into configuration items.

--- web/availableResources.js
+++ web/availableResources.js
@@ -1,8 +1,8 @@
-[
+var availableResources = [
     {"id": "genesyscloud_architect_datatable", "displayName": "Data Tables"},
     {"id": "genesyscloud_flow", "displayName": "Flows"},
     {"id": "genesyscloud_routing_queue", "displayName": "Queues"},
     {"id": "genesyscloud_routing_skill", "displayName": "Skills"},
     {"id": "genesyscloud_user", "displayName": "Users"},
     {"id": "genesyscloud_user_roles", "displayName": "User Roles"}
-]
+];
