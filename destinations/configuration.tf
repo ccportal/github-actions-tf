7c37fbdb4b268b2e306a9a1928b82c65b40e822e
PierrickI3 <pierrick.lozach@inin.com>
2023-07-07 06:19:02 -0400

    Remove version requirement

--- destinations/configuration.tf
+++ destinations/configuration.tf
@@ -16,7 +16,6 @@ variable "TF_VAR_REGION" {
 
 
 provider "genesyscloud" {
-  version            = "~> 1.15.2"
   oauthclient_id     = var.TF_VAR_ID
   oauthclient_secret = var.TF_VAR_SECRET
   aws_region         = var.TF_VAR_REGION
